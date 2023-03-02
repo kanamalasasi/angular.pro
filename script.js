a0fa14fa0f02a9c24de89662cf587fafb076e766
unknown <kanamalasasi123@gmail.com>
2023-03-02 17:28:37 +0530

    My html files

---
+++ script.js
@@ -0,0 +1,23 @@
+
+    alert('hello hiii');
+    alert(1+
+        2
+        +3
+        );
+        let message;
+        message='sasi';
+        message='suresh';
+        alert(message);
+        let hello='sasi kanamala';
+        let hiii='abhinay kanamala';
+        hiii=hello;
+        alert(hiii);
+        alert(hello);
+
+           const my_birthday='22-07-2001';
+           alert(my_birthday);
+           let admin;
+           let name='john';
+           admin=name;
+           alert(admin);
+
